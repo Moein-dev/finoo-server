dfafef3b13d2ab5029dd533dc49dbaa3f544de1b
Moein-dev <moeinmoradi.dev@gmail.com>
2025-03-04 01:31:27 +0330

    Update import syntax for verifyToken middleware
    - Modify import statement for verifyToken to use object destructuring
    - Maintain existing authentication logic in data routes

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const verifyToken = require("../middleware/authMiddleware");
+const { verifyToken } = require("../middleware/authMiddleware");
 const { sendSuccessResponse, sendErrorResponse } = require("../utils/responseHandler");
 const { 
     getTodayData, getAllData, getDataInRange,
