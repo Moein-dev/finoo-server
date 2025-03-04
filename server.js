7ed2e2fa7f8b4b7a818f42ea08799b9af16d1ae7
Moein-dev <moeinmoradi.dev@gmail.com>
2025-03-04 16:14:55 +0330

    update express limit

--- server.js
+++ server.js
@@ -51,8 +51,9 @@ const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per 15 minutes
     message: "Too many requests from this IP, please try again later.",
-    onLimitReached: (req) => {
+    handler: (req, res, next, options) => {
         logger.warn(`⚠️ Rate limit exceeded for IP: ${req.ip}`);
+        res.status(options.statusCode).send(options.message);
     }
 });
 app.use(limiter);
