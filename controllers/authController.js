6ede05d6ffeb2caf2c2bfb58f87586a728498ff0
Moein-dev <moeinmoradi.dev@gmail.com>
2025-08-07 02:56:35 +0330

    Add getPhoneVerification function to authController to support phone verification process

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,7 @@ const {
   updateUserRefreshToken,
   getUserByPhone,
   createPhoneVerification,
+  getPhoneVerification,
 } = require("../services/databaseService");
 const {
   sendSuccessResponse,
