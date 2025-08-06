44f42be32223318b2b1ea3c9f09230a66867b01b
Moein-dev <moeinmoradi.dev@gmail.com>
2025-08-07 03:03:01 +0330

    Update OTP code generation in requestLoginOtp function to use a 5-digit code for enhanced security

--- controllers/authController.js
+++ controllers/authController.js
@@ -105,7 +105,7 @@ exports.requestLoginOtp = async (req, res) => {
       return sendErrorResponse(res, 404, "کاربری با این شماره تایید شده یافت نشد.");
     }
 
-    const code = Math.floor(100000 + Math.random() * 900000).toString();
+    const code = Math.floor(10000 + Math.random() * 90000).toString();
     const expiresAt = new Date(Date.now() + 5 * 60 * 1000); // ۵ دقیقه
 
     await createPhoneVerification(user.id, phone, code, expiresAt);
